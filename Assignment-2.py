e0df16c619bb97d4cd26eca912e05e9ebc03f62b
Rishi Sahu <rishisahu4204@gmail.com>
2025-07-08 17:02:06 +0530

    Update Assignment-2.py

--- Assignment-2.py
+++ Assignment-2.py
@@ -11,6 +11,6 @@ else:
 
 #Task 2: Sum of Integers from 1 to 50 Using a Loop
 sum = 0
-for x in range(1,50):
+for x in range(1,51):
     sum = sum + x ;
 print("The sum of numbers from 1to 50 is: ", sum)
