f4e29bda17747f9c18c9e75da5e7964c673504ea
Rishi Sahu <rishisahu4204@gmail.com>
2025-07-08 10:27:17 +0530

    Add files via upload

---
+++ Assignment-2.py
@@ -0,0 +1,16 @@
+#Module 3: Control Structures in Python
+#Task 1: Check if a Number is Even or Odd
+
+no=int(input("Enter a no.: "))
+a=no%2
+if (a==0):
+    print( no , " is an even no.")
+else:
+    print(no , " is an odd no.\n\n\n")
+
+
+#Task 2: Sum of Integers from 1 to 50 Using a Loop
+sum = 0
+for x in range(1,50):
+    sum = sum + x ;
+print("The sum of numbers from 1to 50 is: ", sum)
